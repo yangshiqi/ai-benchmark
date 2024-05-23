e4e160bf40f02c5ae173f4e0496243957c87d0c7
ysq <yangshiqi1089@gmail.com>
2024-05-23 16:05:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import ai_benchmark
 
 def main():
     benchmark = ai_benchmark.AIBenchmark()
-    results = benchmark.run_training(precision="high")
+    #results = benchmark.run_training(precision="high")
+    #results = benchmark.run_inference(precision="normal")
+
+    #run_inference + run_training on precision="normal"
+    results = benchmark.run()
 
 
 
